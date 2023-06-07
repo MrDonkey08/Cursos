96c46d5583b3c277e5d4649326645d8b0af5376f
Alan <113253976+MrDonkey08@users.noreply.github.com>
2023-06-07 13:38:36 -0600

    Adición de la selección de ataques

--- Programación/Programación-básica/Programas/mokepon/js/mokepon.js
+++ Programación/Programación-básica/Programas/mokepon/js/mokepon.js
@@ -1,8 +1,17 @@
+let playersAttack // it doesn't belong to a function so it's a global variable and can be accessed anywhere in this file
+let enemysAttack
+
 function startGame(){
     let btnPet = document.getElementById('btn-pet')
     btnPet.addEventListener('click', selectPets)
-    
 
+    let btnFire = document.getElementById('btn-fire')
+    let btnWater = document.getElementById('btn-water')
+    let btnGround = document.getElementById('btn-ground')
+
+    btnFire.addEventListener('click', fireAttack)
+    btnWater.addEventListener('click', waterAttack)
+    btnGround.addEventListener('click', groundAttack)
 }
 
 function selectPets(){
@@ -45,6 +54,34 @@ function selectEnemysPet(){
     }
 
     alert("Enemy's pet is " + pets.innerHTML)
+}
+
+function waterAttack(){
+    playersAttack = 'water'
+    selectEnemysAttack()
+}
+
+function fireAttack(){
+    playersAttack = 'fire'
+    selectEnemysAttack()
+}
+
+function groundAttack(){
+   playersAttack = 'ground'
+   selectEnemysAttack() 
+}
+
+function selectEnemysAttack(){
+    let opt = randomNum(1, 3)
+
+    switch(opt){
+        case 1: enemysAttack = 'water'
+            break
+        case 2: enemysAttack = 'fire'
+            break
+        default: enemysAttack = 'ground' // I set default instead of 'case 3' because 'ground' is the only left value that 'enemysAttack' can take
+            break
+    }
 }
 
 window.addEventListener('load', startGame)
