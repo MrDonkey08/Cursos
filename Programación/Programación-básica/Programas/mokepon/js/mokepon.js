d0ec7cda8a1036ac74beb7f08dd2b889ecf4a788
Alan <113253976+MrDonkey08@users.noreply.github.com>
2023-06-07 12:46:46 -0600

    Adición de la función 'enemysPet'

--- Programación/Programación-básica/Programas/mokepon/js/mokepon.js
+++ Programación/Programación-básica/Programas/mokepon/js/mokepon.js
@@ -1,10 +1,11 @@
 function startGame(){
     let btnPet = document.getElementById('btn-pet')
-btnPet.addEventListener('click', selectPet)
+    btnPet.addEventListener('click', selectPets)
+    
 
 }
 
-function selectPet(){
+function selectPets(){
     let pet = document.getElementById('players-pet')
 
     if (document.getElementById('hipodoge').checked){ // .checked returns true if the element is selected
@@ -19,11 +20,31 @@ function selectPet(){
 
     if (pet.innerHTML != ""){
         alert("You choose " + pet.innerHTML + ".")
+        selectEnemysPet()
     }
     else{
         alert("You haven't choose a pet.")
     }
 }
 
+function randomNum(min, max){ // returns a value between min and max
+    return Math.floor(Math.random() * (max - min + 1) + min) // Math.floor truncate (cut) the the decimal part and Math.random() returns a decimal number between 0 and 1.
+}
+
+function selectEnemysPet(){
+    let pets = document.getElementById('enemys-pet')
+    let opt = randomNum(1, 3)
+    
+    switch(opt){
+        case 1: pets.innerHTML = 'Hipodoge'
+            break
+        case 2: pets.innerHTML = 'Capipe'
+            break
+        case 3: pets.innerHTML = 'Ratike'
+            break
+    }
+
+    alert("Enemy's pet is " + pets.innerHTML)
+}
 
 window.addEventListener('load', startGame)
