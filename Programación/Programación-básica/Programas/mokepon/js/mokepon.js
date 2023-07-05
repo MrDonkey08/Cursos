6fdac8683a9af4202442a4dda3b8331be4d9c6ce
Alan <113253976+MrDonkey08@users.noreply.github.com>
2023-07-05 10:00:47 -0600

    Identation to tabs

--- Programación/Programación-básica/Programas/mokepon/js/mokepon.js
+++ Programación/Programación-básica/Programas/mokepon/js/mokepon.js
@@ -5,73 +5,73 @@ let playersLives
 let enemysLives
 
 function startGame(){
-    document.getElementById('select-attack').style.display = 'none'
-    document.getElementById('btn-restart').style.display = 'none'
-    document.getElementById('battle').style.display = 'none'
-    document.getElementById('messages').style.display = 'none'
+	document.getElementById('select-attack').style.display = 'none'
+	document.getElementById('btn-restart').style.display = 'none'
+	document.getElementById('battle').style.display = 'none'
+	document.getElementById('messages').style.display = 'none'
 
-    document.getElementById('btn-restart').addEventListener('click', restartGame)
-    document.getElementById('btn-mokepon').addEventListener('click', selectmokepons)
+	document.getElementById('btn-restart').addEventListener('click', restartGame)
+	document.getElementById('btn-mokepon').addEventListener('click', selectmokepons)
 
-    document.getElementById('btn-fire').addEventListener('click', fireAttack)
-    document.getElementById('btn-water').addEventListener('click', waterAttack)
-    document.getElementById('btn-ground').addEventListener('click', groundAttack)
+	document.getElementById('btn-fire').addEventListener('click', fireAttack)
+	document.getElementById('btn-water').addEventListener('click', waterAttack)
+	document.getElementById('btn-ground').addEventListener('click', groundAttack)
 }
 
 function selectmokepons(){
-    let mokepon = document.getElementById('players-mokepon')
+	let mokepon = document.getElementById('players-mokepon')
 
-    if (document.getElementById('acynonyx').checked){ // .checked returns true if the element is selected
-        mokepon.innerHTML = 'Acynonyx'
-    }
-    else if (document.getElementById('piwith').checked){
-        mokepon.innerHTML = 'Piwith'
-    }
-    else if (document.getElementById('berry').checked){
-        mokepon.innerHTML = 'Berry'
-    }
+	if (document.getElementById('acynonyx').checked){ // .checked returns true if the element is selected
+		mokepon.innerHTML = 'Acynonyx'
+	}
+	else if (document.getElementById('piwith').checked){
+		mokepon.innerHTML = 'Piwith'
+	}
+	else if (document.getElementById('berry').checked){
+		mokepon.innerHTML = 'Berry'
+	}
 
-    if (mokepon.innerHTML != ""){
-        alert("You choose " + mokepon.innerHTML + ".")
-        selectEnemysmokepon()
-        document.getElementById('select-mokepon').style.display = 'none'
-        document.getElementById('select-attack').style.display = 'flex'
-        document.getElementById('battle').style.display= 'flex'
+	if (mokepon.innerHTML != ""){
+		alert("You choose " + mokepon.innerHTML + ".")
+		selectEnemysmokepon()
+		document.getElementById('select-mokepon').style.display = 'none'
+		document.getElementById('select-attack').style.display = 'flex'
+		document.getElementById('battle').style.display= 'flex'
 
-    }
-    else{
-        alert("You haven't choose a mokepon.")
-    }
+	}
+	else{
+		alert("You haven't choose a mokepon.")
+	}
 }
 
 function randomNum(min, max){ // returns a value between min and max
-    return Math.floor(Math.random() * (max - min + 1) + min) // Math.floor truncate (cut) the the decimal part and Math.random() returns a decimal number between 0 and 1.
+	return Math.floor(Math.random() * (max - min + 1) + min) // Math.floor truncate (cut) the the decimal part and Math.random() returns a decimal number between 0 and 1.
 }
 
 function selectEnemysmokepon(){
-    let mokepons = document.getElementById('enemys-mokepon')
-    let opt = randomNum(1, 3)
-    
-    switch(opt){
-        case 1: mokepons.innerHTML = 'Acynonyx'
-            break
-        case 2: mokepons.innerHTML = 'Piwith'
-            break
-        case 3: mokepons.innerHTML = 'Berry'
-            break
-    }
+	let mokepons = document.getElementById('enemys-mokepon')
+	let opt = randomNum(1, 3)
+	
+	switch(opt){
+		case 1: mokepons.innerHTML = 'Acynonyx'
+			break
+		case 2: mokepons.innerHTML = 'Piwith'
+			break
+		case 3: mokepons.innerHTML = 'Berry'
+			break
+	}
 
-    alert("Enemy's mokepon is " + mokepons.innerHTML)
+	alert("Enemy's mokepon is " + mokepons.innerHTML)
 }
 
 function waterAttack(){
-    playersAttack = 'Water'
-    selectEnemysAttack()
+	playersAttack = 'Water'
+	selectEnemysAttack()
 }
 
 function fireAttack(){
-    playersAttack = 'Fire'
-    selectEnemysAttack()
+	playersAttack = 'Fire'
+	selectEnemysAttack()
 }
 
 function groundAttack(){
@@ -80,97 +80,97 @@ function groundAttack(){
 }
 
 function selectEnemysAttack(){
-    let opt = randomNum(1, 3)
+	let opt = randomNum(1, 3)
 
-    switch(opt){
-        case 1: enemysAttack = 'Water'
-            break
-        case 2: enemysAttack = 'Fire'
-            break
-        default: enemysAttack = 'Ground' // I set default instead of 'case 3' because 'Ground' is the only left value that 'enemysAttack' can take
-            break
-    }
-    battle()
+	switch(opt){
+		case 1: enemysAttack = 'Water'
+			break
+		case 2: enemysAttack = 'Fire'
+			break
+		default: enemysAttack = 'Ground' // I set default instead of 'case 3' because 'Ground' is the only left value that 'enemysAttack' can take
+			break
+	}
+	battle()
 }
 
 function battle(){
-    lives()
-    
-    if((enemysLives.innerHTML > 0) && (playersLives.innerHTML > 0)){
-        roundWinner()
-        createMessage()
-    }
-    
-    if(enemysLives.innerHTML <= 0){
-        battleResult("Congrulations! You win. You're the champion!")
-    }
-    else if(playersLives.innerHTML <= 0){
-        battleResult('You lost the battle, but not the war. Keep going')
-    }
+	lives()
+	
+	if((enemysLives.innerHTML > 0) && (playersLives.innerHTML > 0)){
+		roundWinner()
+		createMessage()
+	}
+	
+	if(enemysLives.innerHTML <= 0){
+		battleResult("Congrulations! You win. You're the champion!")
+	}
+	else if(playersLives.innerHTML <= 0){
+		battleResult('You lost the battle, but not the war. Keep going')
+	}
 }
 
 function lives(){
-    playersLives = document.getElementById('players-lives')
-    enemysLives = document.getElementById('enemys-lives')
+	playersLives = document.getElementById('players-lives')
+	enemysLives = document.getElementById('enemys-lives')
 }
 
 function createMessage(){ 
-    let playersAttackP = document.createElement('p')
-    playersAttackP.innerHTML = playersAttack
+	let playersAttackP = document.createElement('p')
+	playersAttackP.innerHTML = playersAttack
 
-    document.getElementById('players-card').appendChild(playersAttackP)
+	document.getElementById('players-card').appendChild(playersAttackP)
 
-    let battleMessageP = document.createElement('p')
-    battleMessageP.innerHTML = winnerMessage
+	let battleMessageP = document.createElement('p')
+	battleMessageP.innerHTML = winnerMessage
 
-    document.getElementById('result-card').appendChild(battleMessageP)
+	document.getElementById('result-card').appendChild(battleMessageP)
 
-    let enemysAttackP = document.createElement('p')
-    enemysAttackP.innerHTML = enemysAttack
+	let enemysAttackP = document.createElement('p')
+	enemysAttackP.innerHTML = enemysAttack
 
-    document.getElementById('enemys-card').appendChild(enemysAttackP)
+	document.getElementById('enemys-card').appendChild(enemysAttackP)
 }
 
 function roundWinner(){
-    winnerMessage = 'Win'
+	winnerMessage = 'Win'
 
-    if(playersAttack == enemysAttack){
-        winnerMessage = 'Draw'
-    }
-    else if((playersAttack == 'Water') && (enemysAttack == 'Fire')){
-        enemysLives.innerHTML -= 1
-    }
-    else if((playersAttack == 'Fire') && (enemysAttack == 'Ground')){
-        enemysLives.innerHTML -= 1
-    } 
-    else if((playersAttack == 'Ground') && (enemysAttack == 'Water')){
-        enemysLives.innerHTML -= 1
-    }
-    else{
-        winnerMessage = "Defeat"
-        playersLives.innerHTML -= 1
-    }
+	if(playersAttack == enemysAttack){
+		winnerMessage = 'Draw'
+	}
+	else if((playersAttack == 'Water') && (enemysAttack == 'Fire')){
+		enemysLives.innerHTML -= 1
+	}
+	else if((playersAttack == 'Fire') && (enemysAttack == 'Ground')){
+		enemysLives.innerHTML -= 1
+	} 
+	else if((playersAttack == 'Ground') && (enemysAttack == 'Water')){
+		enemysLives.innerHTML -= 1
+	}
+	else{
+		winnerMessage = "Defeat"
+		playersLives.innerHTML -= 1
+	}
 }
 
 function battleResult(result){
-    let paragraph = document.createElement('p')
-    paragraph.innerHTML = result
+	let paragraph = document.createElement('p')
+	paragraph.innerHTML = result
 
-    document.getElementById('messages').appendChild(paragraph)
+	document.getElementById('messages').appendChild(paragraph)
 
-    disableAttackButtons()
-    document.getElementById('messages').style.display = 'flex'
-    document.getElementById('btn-restart').style.display = 'block'
+	disableAttackButtons()
+	document.getElementById('messages').style.display = 'flex'
+	document.getElementById('btn-restart').style.display = 'block'
 }
 
 function disableAttackButtons(){
-    document.getElementById('btn-fire').disabled = true
-    document.getElementById('btn-water').disabled = true
-    document.getElementById('btn-ground').disabled = true
+	document.getElementById('btn-fire').disabled = true
+	document.getElementById('btn-water').disabled = true
+	document.getElementById('btn-ground').disabled = true
 }
 
 function restartGame(){
-    location.reload()
+	location.reload()
 }
 
 window.addEventListener('load', startGame)
