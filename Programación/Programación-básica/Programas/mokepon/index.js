7ae66a817d09139f1718e518306e442f520a6d55
Alan <113253976+MrDonkey08@users.noreply.github.com>
2023-07-29 14:44:04 -0600

    Express.js y fetch: API REST con JavaScript

--- Programación/Programación-básica/Programas/mokepon/index.js
+++ Programación/Programación-básica/Programas/mokepon/index.js
@@ -1,9 +1,22 @@
 const express = require("express") // express library import
-
 const app = express()
+const players = []
 
-app.get("/", (req, res) => { // where req is the request and res the object whe use to manages the responses
-	res.send("Hola")
+class Player {
+	constructor(id){
+		this.id = id
+	}
+}
+
+app.get("/join", (req, res) => { // where req is the request and res the object whe use to manages the responses
+	const id = `${Math.random()}`
+	const player = new Player(id)
+
+	players.push(player)
+
+	res.setHeader("Access-Control-Allow-Origin", "*")
+	
+	res.send(id)
 }) // .get for client's requests
 
 app.listen(8080, () => {
