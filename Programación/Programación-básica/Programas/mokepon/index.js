a1b8b4900515743fb14e600a4ff7135b5f6075eb
Alan <113253976+MrDonkey08@users.noreply.github.com>
2023-07-29 13:55:22 -0600

    Servidor web con Express.js

--- Programación/Programación-básica/Programas/mokepon/index.js
+++ Programación/Programación-básica/Programas/mokepon/index.js
@@ -1,1 +1,11 @@
-console.log("¡Hola, mundo!")
+const express = require("express") // express library import
+
+const app = express()
+
+app.get("/", (req, res) => { // where req is the request and res the object whe use to manages the responses
+	res.send("Hola")
+}) // .get for client's requests
+
+app.listen(8080, () => {
+	console.log("Server running")
+})
